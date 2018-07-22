a9d27f480f86017c3f0f34d517b0e7d8dfc60574
pritamSankhari <33205478+pritamSankhari@users.noreply.github.com>
2018-07-22 12:37:38 +0530

    Add files via upload
    Adding new division in index.html file,
    adding new record form in index.html file,
    adding formValidation.js file,
    adding newRecord.php file,
    updating checkSolved() in gameplay.js file,
    updating style.css file

---
+++ newRecord.php
@@ -0,0 +1,6 @@
+<h2><?php
+// echo $_POST['new-name'];
+// echo $_POST['playtime'];
+echo "We are not saving your Record. Goodbye! :)";
+echo "<br><br><h3>Database will be created soon !</h3>";
+?><h2>
